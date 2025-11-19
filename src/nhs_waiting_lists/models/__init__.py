8684774bbabc8910bae27a7a000f5847af64a044
Tom Hodder <tom@limepepper.co.uk>
2025-11-19 05:36:03 +0000

    checkpoint

--- src/nhs_waiting_lists/models/__init__.py
+++ src/nhs_waiting_lists/models/__init__.py
@@ -2,6 +2,5 @@ from .consolidated import Consolidated
 from .t_v_consolidated import VConsolidated
 from .t_v_providers import t_providers
 from .provider import Provider
-from .all_rtt import AllRtt
 from .all_rtt_raw import AllRttRaw
 from .outpatients_activity import OutpatientsActivity
